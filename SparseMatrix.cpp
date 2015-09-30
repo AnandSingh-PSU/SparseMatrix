27be294575c956c4c417bd82a28d08e8acd2624e
ALEXANDER PLETZER <axp5530@psu.edu>
2015-09-30 17:43:20 -0400

    fixing operator+

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -29,12 +29,18 @@ SparseMatrix::getElement(size_t i, size_t j) const {
         }
 }
 
-double
-SparseMatrix::operator+(double SparseMatrix& b) {
+SparseMatrix
+SparseMatrix::operator+(const SparseMatrix& b) {
 	SparseMatrix sp;
-	for() {
-		for() {
-			sp.setElement() = this->sp.getElement() + b.getElement()
-		}
+	for(std::map< std::vector<size_t>, double >::const_iterator it = this->_elements.begin();
+            it != this->_elements.end(); ++it) {
+		std::map< std::vector<size_t>, double >::const_iterator itB = b._elements.find(it->first);
+		double valueB = 0.0;
+		if (itB != b._elements.end()) {
+                        // found element
+			valueB = itB->second;
+                }
+		sp.setElement(it->first[0], it->first[1], it->second + valueB);
 	}
+	return sp;
 }
